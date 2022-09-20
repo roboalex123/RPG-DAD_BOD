e357dd9c9260c6e1f9210471ada3735fa943ea78
plumbobemerald <tevinheins@gmail.com>
2022-09-19 20:52:31 -0700

    Added Heins to NAMES

--- main.cc
+++ main.cc
@@ -1,5 +1,5 @@
 //Fill out this comment with your names and which bullet points you did
-//Partners: Sayre, Voss
+//Partners: Sayre, Voss, Heins
 //Bullet Points:
 //Extra Credit:
 //URL to cover art and music:
