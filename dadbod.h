1ec5fb0bb651d628f4ccac4dc0fa2bd6e2a5dcc1
plumbobemerald <tevinheins@gmail.com>
2022-09-20 22:20:38 -0700

    Added functions 2-5, correct answers working, incorrect answers exit(1)

--- dadbod.h
+++ dadbod.h
@@ -33,11 +33,185 @@ bool ikeaBook(bool task1){
        			task1 = true;
 				return task1;
 			}
+			else{
+				exit (1);
+			}
+		}
+		else{
+			exit (1);
 		}
 	}
 	else{
+		cout << "That is an option, but is it the best Dad Bod option?" << endl;// how to return player to start of if statement?
 		exit (1);
 	}
 	}
 
+bool tolietClog(bool task2){
+
+    int playerChoice = 0;
+
+    cout << "Okay.... I guess I will build this stupid bookcase" << endl;
+    cout << "How should I start the task: " << endl;
+    cout << "1. Grab another Busch Light?" << endl;
+    cout << "2. Organize tools and parts?" << endl;
+    cout << "3. Watch a DIY YouTube video?" << endl;
+    cin >> playerChoice;
+    //playerChoice = read(cin,"Please select 1, 2, 3:");
+
+
+    if (playerChoice == 1){
+        cout <<"Passed"<< endl;
+        cout << "step 2" << endl;
+        cout <<"1,2,3"<< endl;
+        cin >> playerChoice;
+        if(playerChoice == 2){
+            cout <<"Passed"<< endl;
+            cout << "step 3" << endl;
+            cout <<"1,2,3"<< endl;
+            cin >> playerChoice;
+            if (playerChoice == 3){
+                cout <<"Passed"<< endl;
+                task2 = true;
+                return task2;
+            }
+            else{
+                exit (1);
+            }
+        }
+        else{
+            exit (1);
+        }
+    }
+    else{
+        cout << "That is an option, but is it the best Dad Bod option?" << endl;// how to return player to start of if statement?
+        exit (1);
+    }
+    }
+
+bool mowLawn(bool task3){
+
+    int playerChoice = 0;
+
+    cout << "Okay.... I guess I will build this stupid bookcase" << endl;
+    cout << "How should I start the task: " << endl;
+    cout << "1. Grab another Busch Light?" << endl;
+    cout << "2. Organize tools and parts?" << endl;
+    cout << "3. Watch a DIY YouTube video?" << endl;
+    cin >> playerChoice;
+    //playerChoice = read(cin,"Please select 1, 2, 3:");
+
+
+    if (playerChoice == 1){
+        cout <<"Passed"<< endl;
+        cout << "step 2" << endl;
+        cout <<"1,2,3"<< endl;
+        cin >> playerChoice;
+        if(playerChoice == 2){
+            cout <<"Passed"<< endl;
+            cout << "step 3" << endl;
+            cout <<"1,2,3"<< endl;
+            cin >> playerChoice;
+            if (playerChoice == 3){
+                cout <<"Passed"<< endl;
+                task3 = true;
+                return task3;
+            }
+            else{
+                exit (1);
+            }
+        }
+        else{
+            exit (1);
+        }
+    }
+    else{
+        cout << "That is an option, but is it the best Dad Bod option?" << endl;// how to return player to start of if statement?
+        exit (1);
+    }
+    }
+
+bool honrnetNest(bool task4){
+
+    int playerChoice = 0;
+
+    cout << "Okay.... I guess I will build this stupid bookcase" << endl;
+    cout << "How should I start the task: " << endl;
+    cout << "1. Grab another Busch Light?" << endl;
+    cout << "2. Organize tools and parts?" << endl;
+    cout << "3. Watch a DIY YouTube video?" << endl;
+    cin >> playerChoice;
+    //playerChoice = read(cin,"Please select 1, 2, 3:");
+
+
+    if (playerChoice == 1){
+        cout <<"Passed"<< endl;
+        cout << "step 2" << endl;
+        cout <<"1,2,3"<< endl;
+        cin >> playerChoice;
+        if(playerChoice == 2){
+            cout <<"Passed"<< endl;
+            cout << "step 3" << endl;
+            cout <<"1,2,3"<< endl;
+            cin >> playerChoice;
+            if (playerChoice == 3){
+                cout <<"Passed"<< endl;
+                task4 = true;
+                return task4;
+            }
+            else{
+                exit (1);
+            }
+        }
+        else{
+            exit (1);
+        }
+    }
+    else{
+        cout << "That is an option, but is it the best Dad Bod option?" << endl;// how to return player to start of if statement?
+        exit (1);
+    }
+    }
+
+bool bbq(bool task5){
+
+    int playerChoice = 0;
+
+    cout << "Okay.... I guess I will build this stupid bookcase" << endl;
+    cout << "How should I start the task: " << endl;
+    cout << "1. Grab another Busch Light?" << endl;
+    cout << "2. Organize tools and parts?" << endl;
+    cout << "3. Watch a DIY YouTube video?" << endl;
+    cin >> playerChoice;
+    //playerChoice = read(cin,"Please select 1, 2, 3:");
+
+
+    if (playerChoice == 1){
+        cout <<"Passed"<< endl;
+        cout << "step 2" << endl;
+        cout <<"1,2,3"<< endl;
+        cin >> playerChoice;
+        if(playerChoice == 2){
+            cout <<"Passed"<< endl;
+            cout << "step 3" << endl;
+            cout <<"1,2,3"<< endl;
+            cin >> playerChoice;
+            if (playerChoice == 3){
+                cout <<"Passed"<< endl;
+                task5 = true;
+                return task5;
+            }
+            else{
+                exit (1);
+            }
+        }
+        else{
+            exit (1);
+        }
+    }
+    else{
+        cout << "That is an option, but is it the best Dad Bod option?" << endl;// how to return player to start of if statement?
+        exit (1);
+    }
+    }
 
