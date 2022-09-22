e2a57138b6e134adf2c9dbdf2ccda0dbffe743f3
Robert Voss <roboalex123@gmail.com>
2022-09-22 16:47:07 -0700

    Pushed correct version of task.h

--- task.h
+++ task.h
@@ -2,18 +2,49 @@
 #include "/public/colors.h"
 using namespace std;
 
+auto [rows,cols] = get_terminal_size();
 
-void setupScreen(string output){
-	auto [rows,cols] = get_terminal_size();
-	clearscreen();
-	movecursor(rows/2,(cols - output.size())/2); 
+int centerX(string output){
+	return(cols - output.size())/2; 
 
 }
 //-Replace flickering light in kitchen
-bool replaceLight(){
-	string output ="Honey please build this book Shelf";
-	setupScreen(output);
-	cout << output << endl;
+bool replaceLight (){
+	int yStart = rows / 2;
+	
+	string output = "Honey please build this book Shelf";
+	movecursor(yStart,centerX(output));
+	cout << output;
+	
+	output = "Pick the right step!";
+	movecursor(++yStart,centerX(output));
+	cout << output;
+	
+	output = "1. Grab another Busch Light?";
+	movecursor(++yStart,centerX(output));
+	cout << output;
+	
+	output = "2. Organize yools and parts?";
+	movecursor(++yStart,centerX(output));
+	cout << output;
+
+	output = "3. Watch a DIY YouTube Video";
+	movecursor(++yStart,centerX(output));
+	cout << output;
+	
+	while(true){
+		int choice = read<int>("Please pick a choice");
+		if (choice == 1){
+			cout <<"This is option 1 working";
+		}
+		else if (choice)
+
+
+	}
+	
+	
+
+	
 	return true;
 }
 
