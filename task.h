724771e64d05bfe89a1e51e35512b37c8f52bb00
Anthony Sayre <anthonysayre@icloud.com>
2022-09-23 13:07:26 -0700

    added working usleep and buffer clearing, also changed return to int

--- task.h
+++ task.h
@@ -9,38 +9,44 @@ int centerX(string output){
 
 }
 //-Replace flickering light in kitchen
-bool ikeaBook(){
+int ikeaBook(){
 	clearscreen();
 	int yStart = rows / 2;
 	int chances = 3;
 
-	string output;
-	output = "Okay.... I guess I will build this stupid bookcase";
+	show_cursor(false);
+	string output = "Okay.... I guess I will build this stupid bookcase";
 	movecursor(yStart,centerX(output));
-	cout << output;
-	usleep(2'500'000);
 
+	cout << output;
+	cout.flush();
+	//char x = read<char>();
+	usleep(2'500'000);
+	
 	while(true){//question 1
-		if (chances <= 0) return false;
+		if (chances <= 0){
+			show_cursor(true);
+			return 1;
+		}
 
 		int inner_yStart = yStart;
 		clearscreen();
 
 		output = "How should I start the task.";
 		movecursor(++inner_yStart,centerX(output));
-		cout << output;
+		cout << output << endl;
 
 		output = "1. Grab another Busch Light?";
 		movecursor(++inner_yStart,centerX(output));
-		cout << output;
+		cout << output << endl;
 
 		output = "2. Organize tools and parts?";
 		movecursor(++inner_yStart,centerX(output));
-		cout << output;
+		cout << output << endl;
 
 		output = "3. Watch a DIY YouTube Video?";
 		movecursor(++inner_yStart,centerX(output));
-		cout << output;
+		cout << output << endl;
 
 		output = "Enter choice number: ";
 		movecursor(++inner_yStart,centerX(output));
@@ -49,15 +55,14 @@ bool ikeaBook(){
 		if (choice == 1){
 			output =  "Now that's thinking like a Dad Bod.";
 			movecursor(++inner_yStart,centerX(output));
-
-			cout << output;
+			cout << output << endl;
 			usleep(2'000'000);
 			break;
 		}
 		else{
 			output = "That is an option, but is it the best Dad Bod option?";
 			movecursor(++inner_yStart,centerX(output));
-			cout << output;
+			cout << output << endl;
 			usleep(2'000'000);
 			chances--;
 			continue;
@@ -66,27 +71,29 @@ bool ikeaBook(){
 	clearscreen();
 	output = "What's next?";
 	movecursor(yStart,centerX(output));
-	cout << output;
+	cout << output << endl;
 	usleep(1'500'000);
 
 
 	while(true){//question 2
-		if (chances <= 0) return false;
-
+		if (chances <= 0){
+			show_cursor(true);
+			return 1;
+		}
 		int inner_yStart = yStart;
 		clearscreen();
 
 		output = "1. Read instruction.";
 		movecursor(inner_yStart,centerX(output));
-		cout << output;
+		cout << output << endl;
 
 		output = "2. Throw away instructions, Dad Bods don't need those.";
 		movecursor(++inner_yStart,centerX(output));
-		cout << output;
+		cout << output << endl;
 
 		output = "3. Call the neighbor for help.";
 		movecursor(++inner_yStart,centerX(output));
-		cout << output;
+		cout << output << endl;
 
 		output = "Enter choice number: ";
 		movecursor(++inner_yStart,centerX(output));
@@ -95,14 +102,14 @@ bool ikeaBook(){
 		if (choice == 2){
 			output =  "Now that's thinking like a Dad Bod.";
 			movecursor(++inner_yStart,centerX(output));
-			cout << output;
+			cout << output << endl;
 			usleep(2'000'000);
 			break;
 		}
 		else{
 			output = "That is an option, but is it the best Dad Bod option?";
 			movecursor(++inner_yStart,centerX(output));
-			cout << output;
+			cout << output << endl;
 			usleep(2'000'000);
 			chances--;
 			continue;
@@ -111,24 +118,27 @@ bool ikeaBook(){
 	clearscreen();
 	output = "It's completed, how did I do?";
 	movecursor(yStart,centerX(output));
-	cout << output;
+	cout << output << endl;
 	usleep(1'500'000);
 
 	while(true){//question 3
-		if (chances <= 0) return false;
+		if (chances <= 0){
+			show_cursor(true);
+			return 1;
+		}
 		int inner_yStart = yStart;
 		clearscreen();
 		output = "1. Perfectlly level and no extra parts.";
 		movecursor(inner_yStart,centerX(output));
-		cout << output;
+		cout << output << endl;
 
 		output = "2. Mostly level and the proper amount of extra parts.";
 		movecursor(++inner_yStart,centerX(output));
-		cout << output;
+		cout << output << endl;
 
 		output = "3. Extra parts and not level is completely normal.";
 		movecursor(++inner_yStart,centerX(output));
-		cout << output;
+		cout << output << endl;
 
 		output = "Enter choice number: ";
 		movecursor(++inner_yStart,centerX(output));
@@ -138,14 +148,14 @@ bool ikeaBook(){
 			output =  "Now that's thinking like a Dad Bod.";
 			movecursor(++inner_yStart,centerX(output));
 
-			cout << output;
+			cout << output << endl;
 			usleep(2'000'000);
 			break;
 		}
 		else{
 			output = "That is an option, but is it the best Dad Bod option?";
 			movecursor(++inner_yStart,centerX(output));
-			cout << output;
+			cout << output << endl;
 			usleep(2'000'000);
 			chances--;
 			continue;
@@ -153,10 +163,11 @@ bool ikeaBook(){
 	}
 	clearscreen();
 	output = "Well done Dad Bod, grab another beer and check that off the list";
-	cout << output;
+	movecursor(yStart,centerX(output));
+	cout << output << endl;
 	usleep(2'500'000);
-
-	return true;
+	show_cursor(true);
+	return 2;
 }
 
 
