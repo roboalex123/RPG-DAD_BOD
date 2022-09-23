7529e4fac1498893a53838ece125152e4081160b
plumbobemerald <tevinheins@gmail.com>
2022-09-22 20:52:22 -0700

    pushing non working Task function

--- task.h
+++ task.h
@@ -9,42 +9,152 @@ int centerX(string output){
 
 }
 //-Replace flickering light in kitchen
-bool replaceLight (){
+bool ikeaBook(){
+	clearscreen();
 	int yStart = rows / 2;
-	
-	string output = "Honey please build this book Shelf";
+	int chances = 3;
+
+	string output = "Okay.... I guess I will build this stupid bookcase";
 	movecursor(yStart,centerX(output));
 	cout << output;
-	
-	output = "Pick the right step!";
-	movecursor(++yStart,centerX(output));
-	cout << output;
-	
-	output = "1. Grab another Busch Light?";
-	movecursor(++yStart,centerX(output));
-	cout << output;
-	
-	output = "2. Organize yools and parts?";
-	movecursor(++yStart,centerX(output));
-	cout << output;
+	usleep(2'500'000);
 
-	output = "3. Watch a DIY YouTube Video";
-	movecursor(++yStart,centerX(output));
-	cout << output;
-	
-	while(true){
-		int choice = read<int>("Please pick a choice");
+	while(true){//question 1
+		if (chances <= 0) return false;
+
+		int inner_yStart = yStart;
+		clearscreen();
+
+		output = "How should I start the task.";
+		movecursor(++inner_yStart,centerX(output));
+		cout << output;
+
+		output = "1. Grab another Busch Light?";
+		movecursor(++inner_yStart,centerX(output));
+		cout << output;
+
+		output = "2. Organize tools and parts?";
+		movecursor(++inner_yStart,centerX(output));
+		cout << output;
+
+		output = "3. Watch a DIY YouTube Video?";
+		movecursor(++inner_yStart,centerX(output));
+		cout << output;
+
+		output = "Enter choice number: ";
+		movecursor(++inner_yStart,centerX(output));
+		int choice = read<int>(output);
+
 		if (choice == 1){
-			cout <<"This is option 1 working";
+			output =  "Now that's thinking like a Dad Bod.";
+			movecursor(++inner_yStart,centerX(output));
+
+			cout << output;
+			usleep(2'000'000);
+			break;
 		}
-		else if (choice)
-
-
+		else{
+			output = "That is an option, but is it the best Dad Bod option?";
+			movecursor(++inner_yStart,centerX(output));
+			cout << output;
+			usleep(2'000'000);
+			chances--;
+			continue;
+		}
 	}
-	
-	
+	clearscreen();
+	output = "What's next?";
+	movecursor(yStart,centerX(output));
+	cout << output;
+	usleep(1'500'000);
 
-	
+
+	while(true){//question 2
+		if (chances <= 0) return false;
+
+		int inner_yStart = yStart;
+		clearscreen();
+
+		output = "1. Read instruction.";
+		movecursor(inner_yStart,centerX(output));
+		cout << output;
+
+		output = "2. Throw away instructions, Dad Bods don't need those.";
+		movecursor(++inner_yStart,centerX(output));
+		cout << output;
+
+		output = "3. Call the neighbor for help.";
+		movecursor(++inner_yStart,centerX(output));
+		cout << output;
+
+		output = "Enter choice number: ";
+		movecursor(++inner_yStart,centerX(output));
+		int choice = read<int>(output);
+
+		if (choice == 2){
+			output =  "Now that's thinking like a Dad Bod.";
+			movecursor(++inner_yStart,centerX(output));
+			cout << output;
+			usleep(2'000'000);
+			break;
+		}
+		else{
+			output = "That is an option, but is it the best Dad Bod option?";
+			movecursor(++inner_yStart,centerX(output));
+			cout << output;
+			usleep(2'000'000);
+			chances--;
+			continue;
+		}
+	}
+	clearscreen();
+	output = "It's completed, how did I do?";
+	movecursor(yStart,centerX(output));
+	cout << output;
+	usleep(1'500'000);
+
+	while(true){//question 3
+		if (chances <= 0) return false;
+		int inner_yStart = yStart;
+		clearscreen();
+		output = "1. Perfectlly level and no extra parts.";
+		movecursor(inner_yStart,centerX(output));
+		cout << output;
+
+		output = "2. Mostly level and the proper amount of extra parts.";
+		movecursor(++inner_yStart,centerX(output));
+		cout << output;
+
+		output = "3. Extra parts and not level is completely normal.";
+		movecursor(++inner_yStart,centerX(output));
+		cout << output;
+
+		output = "Enter choice number: ";
+		movecursor(++inner_yStart,centerX(output));
+		int choice = read<int>(output);
+
+		if (choice == 3){
+			output =  "Now that's thinking like a Dad Bod.";
+			movecursor(++inner_yStart,centerX(output));
+
+			cout << output;
+			usleep(2'000'000);
+			break;
+		}
+		else{
+			output = "That is an option, but is it the best Dad Bod option?";
+			movecursor(++inner_yStart,centerX(output));
+			cout << output;
+			usleep(2'000'000);
+			chances--;
+			continue;
+		}
+	}
+	clearscreen();
+	output = "Well done Dad Bod, grab another beer and check that off the list";
+	cout << output;
+	usleep(2'500'000);
+
 	return true;
 }
 
--- test.cc
+++ test.cc
@@ -1,5 +1,5 @@
 #include "task.h"
 
 int main(){
-	cout << replaceLight();
+	cout << ikeaBook();
 }
