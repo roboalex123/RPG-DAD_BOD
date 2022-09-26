f4e8e9af838e4e223a946a2d0228b6a9eb160356
plumbobemerald <tevinheins@gmail.com>
2022-09-25 18:32:37 -0700

    Fixed combat.h round 1

--- combat.h
+++ combat.h
@@ -1,33 +1,42 @@
-#include "/public/read.h"
-#include "/public/colors.h"
 #include <cmath>
 #include <cctype>
-#include <iostream>
+#include "task.h"
+#include "time.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
 using namespace std;
 
 // NOTE: This is just a skeleton of what this might look like 
 //       Please change anything that could be made better or
 //       that's need to make it work in main
 
-auto [rows,cols] = get_terminal_size();
+ 
+int dadh = 100, wifeh  = 100l;
+int goalRand = 0;
+int dadRand = 0;
+int wifeRand = 0;
+int baseDamage = 10;
+int lowBuff = -3;
+int medBuff = 3;
+int bigBuff = 6;
+int maxDamage = 40;
 
-int centerX(string output){
-	return(cols - output.size())/2; 
-}
 int round1(int& dadh, int& wifeh){
+srand(time(0));
 	clearscreen();
 	int yStart = rows / 2;
-	int chances = 3;
 
 	show_cursor(false);
-	string output = "ROUND 1";
+	string output = "Angry wife appears out of no where. You can tell by the look on her face she wants to fight.";
 	movecursor(yStart,centerX(output));
 
 	cout << output;
 	cout.flush();
 	//char x = read<char>();
 	usleep(2'500'000);
-	
+
+
 	while(true){//question 1
 
 		int inner_yStart = yStart;
@@ -37,7 +46,6 @@ int round1(int& dadh, int& wifeh){
 			output = "Fail";
 			movecursor(++inner_yStart,centerX(output));
 			cout << output << endl;
-			exit(1);
 		}
 
 		output = "What the hell did you do to that book case!?";
@@ -52,183 +60,82 @@ int round1(int& dadh, int& wifeh){
 		movecursor(++inner_yStart,centerX(output));
 		cout << output << endl;
 
-		output = "3. ";
+		output = "3.[HONEST] I gave it my best shot okay...";
 		movecursor(++inner_yStart,centerX(output));
 		cout << output << endl;
 
 		output = "Enter choice number: ";
 		movecursor(++inner_yStart,centerX(output));
 		int choice = read<int>(output);
+		if (choice < 1 or choice > 3){
+			output ="Please enter a valid choice!";
+			movecursor(++inner_yStart,centerX(output));
+			cout << output << endl; 
+			continue;
+		}
+		
+		dadRand  = (rand() % 100) + 1;
+		wifeRand = (rand() % 100) + 1;
 
-		if (choice == 1){
-			output =  "SUPER EFFECTIVE";
+		if (dadRand >= wifeRand){
+			if (choice == 1){
+			output =  "SUPER EFFECTIVE TO WIFE";
 			movecursor(++inner_yStart,centerX(output));
 			cout << output << endl;
 
-			wifeh -= 33;
+			wifeh -= baseDamage + bigBuff + (rand() % maxDamage - (baseDamage + bigBuff) + 1);
+			}
+			
+			if (choice == 3){
+			output =  "EFFECTIVE TO WIFE";
+			movecursor(++inner_yStart,centerX(output));
+			cout << output << endl;
+
+			wifeh -= baseDamage + medBuff + (rand() % maxDamage - (baseDamage + medBuff) + 1);
+			}
+
+			if (choice == 2){
+			output =  "NOT VERY EFFECTIVE TO WIFE";
+			movecursor(++inner_yStart,centerX(output));
+			cout << output << endl;
+
+			wifeh -= baseDamage + lowBuff + (rand() % maxDamage - (baseDamage + lowBuff) + 1);
+			}
+			usleep(2'000'000);
 			return wifeh;
 
-			usleep(2'000'000);
 			break;
 		}
 		else{
-			output = "Is that really the best response?";
+			if (choice == 1){
+			output =  "NOT VERY EFFECTIVE TO DAD BOD";
 			movecursor(++inner_yStart,centerX(output));
 			cout << output << endl;
+
+			dadh -= baseDamage + lowBuff + (rand() % maxDamage - (baseDamage + lowBuff) + 1);
+			}
+			
+			if (choice == 3){
+			output =  "EFFECTIVE TO DAD BOD";
+			movecursor(++inner_yStart,centerX(output));
+			cout << output << endl;
+
+			dadh -= baseDamage + medBuff + (rand() % maxDamage - (baseDamage + medBuff) + 1);
+			}
+
+			if (choice == 2){
+			output =  "SUPER EFFECTIVE DAD BOD";
+			movecursor(++inner_yStart,centerX(output));
+			cout << output << endl;
+
+			dadh -= baseDamage + bigBuff + (rand() % maxDamage - (baseDamage + bigBuff) + 1);
+			}
 			usleep(2'000'000);
-			dadh -= 25;
+			
 			return dadh;
-			continue;
-		}
-	}
-	clearscreen();
-	output = "ROUND 2";
-	movecursor(yStart,centerX(output));
-	cout << output << endl;
-	usleep(1'500'000);
-}
-int round2(int& dadh, int& wifeh){
-	clearscreen();
-	int yStart = rows / 2;
-	int chances = 3;
-
-	show_cursor(false);
-	string output = "ROUND 2";
-	movecursor(yStart,centerX(output));
-
-	cout << output;
-	cout.flush();
-	//char x = read<char>();
-	usleep(2'500'000);
-	
-	while(true){//question 1
-
-		int inner_yStart = yStart;
-		clearscreen();
-
-	if (dadh <= 0){
-			output = "Fail";
-			movecursor(++inner_yStart,centerX(output));
-			cout << output << endl;
-			exit(1);
-		}
-
-
-		output = "What the hell did you do to that book case!?";
-		movecursor(++inner_yStart,centerX(output));
-		cout << output << endl;
-
-		output = "1. [RECEIPT] Why did you spend $200 dollars on it? I thought you said it was only $100?";
-		movecursor(++inner_yStart,centerX(output));
-		cout << output << endl;
-
-		output = "2. [LIE] What? I followed the instructions!";
-		movecursor(++inner_yStart,centerX(output));
-		cout << output << endl;
-
-		output = "3. ";
-		movecursor(++inner_yStart,centerX(output));
-		cout << output << endl;
-
-		output = "Enter choice number: ";
-		movecursor(++inner_yStart,centerX(output));
-		int choice = read<int>(output);
-
-		if (choice == 1){
-			output =  "SUPER EFFECTIVE";
-			movecursor(++inner_yStart,centerX(output));
-			cout << output << endl;
-			wifeh -= 33;
-			return wifeh;
-			usleep(2'000'000);
 			break;
 		}
-		else{
-			output = "Is that really the best response?";
-			movecursor(++inner_yStart,centerX(output));
-			cout << output << endl;
-			usleep(2'000'000);
-			dadh -= 25;
-			return dadh;
-			continue;
-		}
 	}
-	clearscreen();
-	output = "ROUND 3";
-	movecursor(yStart,centerX(output));
-	cout << output << endl;
-	usleep(1'500'000);
 }
-int round3(int& dadh, int& wifeh){
-	clearscreen();
-	int yStart = rows / 2;
-	int chances = 3;
-
-	show_cursor(false);
-	string output = "ROUND 3";
-	movecursor(yStart,centerX(output));
-
-	cout << output;
-	cout.flush();
-	//char x = read<char>();
-	usleep(2'500'000);
-	
-	while(true){//question 1	
-
-		int inner_yStart = yStart;
-		clearscreen();
-
-	if (dadh <= 0){
-			output = "Fail";
-			movecursor(++inner_yStart,centerX(output));
-			cout << output << endl;
-			exit(1);
-		}
-
-		output = "What the hell did you do to that book case!?";
-		movecursor(++inner_yStart,centerX(output));
-		cout << output << endl;
-
-		output = "1. [RECEIPT] Why did you spend $200 dollars on it? I thought you said it was only $100?";
-		movecursor(++inner_yStart,centerX(output));
-		cout << output << endl;
-
-		output = "2. [LIE] What? I followed the instructions!";
-		movecursor(++inner_yStart,centerX(output));
-		cout << output << endl;
-
-		output = "3. ";
-		movecursor(++inner_yStart,centerX(output));
-		cout << output << endl;
-
-		output = "Enter choice number: ";
-		movecursor(++inner_yStart,centerX(output));
-		int choice = read<int>(output);
-
-		if (choice == 1){
-			output =  "SUPER EFFECTIVE";
-			movecursor(++inner_yStart,centerX(output));
-			cout << output << endl;
-			wifeh -= 34;
-			return wifeh;
-			usleep(2'000'000);
-			break;
-		}
-		else{
-			output = "Is that really the best response?";
-			movecursor(++inner_yStart,centerX(output));
-			cout << output << endl;
-			usleep(2'000'000);
-			dadh -= 25;
-			return dadh;
-			continue;
-		}
-	}
-	clearscreen();
-	output = "Win!";
-	movecursor(yStart,centerX(output));
-	cout << output << endl;
-	usleep(1'500'000);
-
-}
-
+//you are acting just like your mother (Walks away)
+// You peaked in high school (Walks away)
